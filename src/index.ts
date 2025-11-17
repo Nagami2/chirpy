264f9b619afd8c0ef9ba33bf6c39f923d0574971
Nagami2 <nagababukatakam2@gmail.com>
2025-11-17 13:45:47 -0500

    added custom handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 
+import { Request, Response } from "express";
+
 const app = express();
 const PORT = 8080;
 
-//tell express: "serve any files found in the current directory"
+// --- custom handlers ---
+const handlerReadiness = (req: Request, res: Response) => {
+  res.status(200);
+  res.set("Content-Type", "text/plain; charset=utf-8"); // set the header
+  res.send("OK"); // send the body text
+};
+
+app.get("/healthz", handlerReadiness);
+
+//updated static files path
 app.use(express.static("."));
 
 //listen port 8080
