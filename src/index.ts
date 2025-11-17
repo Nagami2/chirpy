220f14184d53b7e48a19c664d4443246d5aa4006
Nagami2 <nagababukatakam2@gmail.com>
2025-11-17 13:36:21 -0500

    serving static files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
+const PORT = 8080;
+
+//tell express: "serve any files found in the current directory"
+app.use(express.static("."));
 
 //listen port 8080
 app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
-})
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
